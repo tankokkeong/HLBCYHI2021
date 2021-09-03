ab6f2baa3d033e35eb1c7ebde39cfc05633e7f75
TAN KOK KEONG <tankokkeong4488@gmail.com>
2021-09-03 22:04:47 +0800

    login page

--- login.php
+++ login.php
@@ -18,9 +18,44 @@
 
     <div class="body-content">
         <div class="login-body">
-            <div class="login-img">
-                <img src="images/mobile-login-rafiki.png" />
+            <div class="row login-row">
+                <div class="col-lg-6 login-col login-col-1">
+                    <div class="login-img">
+                        <img src="images/financial data-amico.png" id="login-image"/>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 login-col login-col-2">
+                    <div class="mt-3 text-center mb-3">
+                        <h1>Sign In</h1>
+                    </div>
+
+                    <div class="mt-3 mb-3 text-secondary text-center">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    </div>
+
+                    <form class="mt-3 login-form">
+                        <div class="form-group">
+                            <input type="email" class="input-control" placeholder="Email address">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="input-control" placeholder="Password">
+                        </div>
+
+                        <div class="row">
+                            <div class="col">
+                                <a href="sign-up">Do not have an account?</a>
+                            </div>
+
+                            <div class="col text-right">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
             </div>
+            
         </div>
     </div>
 
