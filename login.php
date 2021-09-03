e955816e3459f16c32b4ce31f913dd1d4784ca8e
TAN KOK KEONG <tankokkeong4488@gmail.com>
2021-09-03 19:22:15 +0800

    file structure

--- login.php
+++ login.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
 
+    <?php 
+        include 'plugins.php';
+    ?>
+    <title>Login</title>
+    <link rel="stylesheet" href="style/login.css"/> 
 </head>
 <body>
     <?php 
@@ -13,7 +17,11 @@
     ?>
 
     <div class="body-content">
-
+        <div class="login-body">
+            <div class="login-img">
+                <img src="images/Mobile-login-rafiki.png" />
+            </div>
+        </div>
     </div>
 
     <?php 
